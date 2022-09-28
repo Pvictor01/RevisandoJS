126352a5bb278986abbd708d63cb20e9face87c4
Paulo Victor <paulovictorlima418@gmail.com>
2022-09-27 23:06:10 -0300

    Operadores

---
+++ operadores/operadores.js
@@ -0,0 +1,18 @@
+//ARITMÉTICOS +, -, *, /, %
+1 + 1;
+10 / 2;
+10 % 3;
+
+//AGRUPAMENTO ()
+(1 + 3) * 2;
+
+//ATRIBUIÇÃO =, +=, -=, /= %=
+let x = 3;
+x += 5; //x = 8
+
+//ENCADEAMENTO
+console.error(); //ponto para encadear algo
+
+//INCREMENTAR ++, --
+let a = 2;
+a++ //a = 3
