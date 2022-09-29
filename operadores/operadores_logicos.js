06d9f7ce0398d824a071439bf019a3c89594f5ef
Paulo Victor <paulovictorlima418@gmail.com>
2022-09-28 23:46:08 -0300

    Operadores Lógicos

---
+++ operadores/operadores_logicos.js
@@ -0,0 +1,9 @@
+//E (AND) Só é verdadeiro de ambas as partes forem true - Operador &&
+true && true // = true
+3 > 2 && 8 !== 9 // = true
+
+//OU (OR) Só é falso se ambas as partes forem falsas - Operador ||
+7 !== 'Victor' || 6 < 2 // = true
+
+//Não (NOT) Inverte o resultado (de verdadeiro para falso e vice-versa) - Operador !
+!10 < 8 // = true
