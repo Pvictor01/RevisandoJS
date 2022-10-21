c0c7ff0528ac045ee7a980e23144a62072ff281f
Paulo Victor <paulovictorlima418@gmail.com>
2022-10-20 23:52:40 -0300

    Exercicio calculo de velocidade

---
+++ exercicios/teste_velocidade.js
@@ -0,0 +1,16 @@
+const veiculoA = prompt('Digite o nome do primeiro veículo');
+const velocidadeA = prompt('Informe a velocidade do primeiro veículo');
+
+const veiculoB = prompt('Digite o nome do segundo veículo');
+const velocidadeB = prompt('Informe a velocidade do segundo veículo');
+
+const velocitA = parseInt(velocidadeA);
+const velocitB = parseInt(velocidadeB);
+
+if(velocitA > velocitB) {
+    alert('O veículo A é mais rápido! \n Veículo A: ' + velocidadeA + 'Km/h' + '\n Veículo B: ' + velocidadeB + 'Km/h');
+} else if(velocitB > velocitA) {
+    alert('O veículo B é mais rápido! \n Veículo B: ' + velocidadeB + 'Km/h' + '\n Veículo A: ' + velocidadeA + 'Km/h');
+} else if(velocitA == velocitB){
+    alert('Os dois veículos estão na mesma velocidade \n Veículo A: ' + velocidadeA + 'Km/h' + '\n Veículo B: ' + velocidadeB + 'Km/h')
+}
