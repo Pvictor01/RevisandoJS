5fc00c700b7257f54f4c0523ca93bc52e1bb0834
Paulo Victor <paulovictorlima418@gmail.com>
2022-12-08 00:28:06 -0300

    Exercicio - Procurando palavras que são palíndromos

---
+++ exercicios/procurando_palindromos.js
@@ -0,0 +1,15 @@
+/*Escreva um programa em javascript que seja capaz de identificar se uma palavra é um palíndromo. Um palíndromo é uma palavra que lida de trás para frente possui as mesmas letras na mesma ordem. O programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem dizendo se aquela palavra é ou não um palíndromo. Caso não seja um palíndromo, o programa deve mostrar a palavra lida da esquerda para direita e da direita para esquerda.*/
+
+const word = prompt('Digite uma palavra');
+
+let invertedWord = '';
+
+for (let i = word.length - 1; i >= 0; i--) {
+  invertedWord += word[i];
+};
+
+if (word == invertedWord) {
+  alert('É um palíndromo' + '\nForma normal: ' + word + '\nForma inversa: ' + invertedWord);
+} else {
+  alert('Não é um palíndromo' + '\nForma normal: ' + word + '\nForma inversa: ' + invertedWord);
+};
