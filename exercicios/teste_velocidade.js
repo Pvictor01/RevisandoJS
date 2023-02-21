e6883044f58b36500193adf08fc31cf6d0cee97e
Paulo Victor <paulovictorlima418@gmail.com>
2023-02-21 20:07:39 -0300

    Exercicio teste de velocidade de veículos

--- exercicios/teste_velocidade.js
+++ exercicios/teste_velocidade.js
@@ -1,16 +1,18 @@
+/*Escreva um programa em javascript que permita inserir o nome e a velocidade de dois veículos e exiba na tela uma mensagem dizendo qual dos dois é mais rápido (ou que as velocidades são iguais se este for o caso)*/
+
 const veiculoA = prompt('Digite o nome do primeiro veículo');
 const velocidadeA = prompt('Informe a velocidade do primeiro veículo');
 
 const veiculoB = prompt('Digite o nome do segundo veículo');
 const velocidadeB = prompt('Informe a velocidade do segundo veículo');
 
-const velocitA = parseInt(velocidadeA);
-const velocitB = parseInt(velocidadeB);
+const velocityA = parseFloat(velocidadeA);
+const velocityB = parseFloat(velocidadeB);
 
-if(velocitA > velocitB) {
-    alert('O veículo A é mais rápido! \n Veículo A: ' + velocidadeA + 'Km/h' + '\n Veículo B: ' + velocidadeB + 'Km/h');
-} else if(velocitB > velocitA) {
-    alert('O veículo B é mais rápido! \n Veículo B: ' + velocidadeB + 'Km/h' + '\n Veículo A: ' + velocidadeA + 'Km/h');
-} else if(velocitA == velocitB){
-    alert('Os dois veículos estão na mesma velocidade \n Veículo A: ' + velocidadeA + 'Km/h' + '\n Veículo B: ' + velocidadeB + 'Km/h')
+if(velocityA > velocityB) {
+  alert('O veículo (A) é mais rápido! \n Veículo A: ' + velocidadeA + 'Km/h' + '\n Veículo B: ' + velocidadeB + 'Km/h');
+} else if(velocityB > velocityA) {
+  alert('O veículo (B) é mais rápido! \n Veículo B: ' + velocidadeB + 'Km/h' + '\n Veículo A: ' + velocidadeA + 'Km/h');
+} else if(velocityA == velocityB){
+  alert('Os dois veículos estão na mesma velocidade \n Veículo A: ' + velocidadeA + 'Km/h' + '\n Veículo B: ' + velocidadeB + 'Km/h')
 };
