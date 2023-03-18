394105529a96b24f32665b989d5dfcffe2877f7b
Paulo Victor <paulovictorlima418@gmail.com>
2023-03-18 17:24:41 -0300

    Revisando o sort

---
+++ funcoes/hofAndArrays/sort.js
@@ -0,0 +1,16 @@
+const characters = [
+  { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+  { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+  { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+  { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+  { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+];
+
+//SORT (modifica o array) - compara os itens do array de 2 em 2
+characters.slice().sort(function(a, b) { // utilizado o slice, não altera o array original
+  return a.nivel - b.nivel; //dando o resultado negativo, o item vai para o final do array, assim ficando de forma crescente
+});
+
+console.log(characters);
