085f45d5875738e01b5cff34463276f16c0bf264
Paulo Victor <paulovictorlima418@gmail.com>
2023-03-21 15:00:08 -0300

    Objetos globais

---
+++ ObjetosGlobais/objetos_globais.js
@@ -0,0 +1,21 @@
+//OBJETOS GLOBAIS
+//Object, Array, String, Number, Date, Math
+
+let person = Object();
+console.log(person);
+
+let list = Array(1, 2, 3);
+console.log(list);
+
+let a = String('a');
+console.log(a);
+
+let number = Number('3');
+console.log(number);
+
+let int = Number.isInteger(45.8);//retorna balor booleano
+console.log(int);
+
+console.log(Date());//retorna a data
+
+console.log(Math.random());//Math serve para trabalhar com expressões matemáticas. ramdon gera numeros aleatórios
